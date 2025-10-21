f41b3845c176130bb09339008c1d2a30a34ced11
dmitry-nazarov22 <dima.nazarov@icloud.com>
2025-10-21 15:13:27 +0300

    Added handleAdd and handleEdit with todo for them (no funtionality yet).

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,23 @@ import AddIcon from "@mui/icons-material/Add";
 import "./App.css";
 
 function App() {
+  let todos = [];
+  let dones = [];
+
+  function handleAdd() {
+    // TODO:
+    // - Open modal with text input, add and close buttons
+    // - After Add is pressed validate and save input into todos
+    // - If validation wasn't succesful or close is pressed – handle
+  }
+
+  function handleEdit() {
+    // TODO:
+    // - Open modal with text input (old text pasted here), edit, delete and close buttons
+    // - After Edit is pressed validate and save input into todos or dones
+    // - If validation wasn't succesful or close is pressed – handle
+  }
+
   return (
     <>
       <div className="wrapper">
@@ -75,6 +92,7 @@ function App() {
         </section>
         <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
           <Button
+            onClick={() => handleAdd()}
             variant="outlined"
             startIcon={<AddIcon />}
             sx={{ color: "white", borderColor: "white" }}
